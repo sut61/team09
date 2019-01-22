24d6fcccfe44dccd1cce2d4086842c90e8cc4e0b
bastonzam <b5906505@gmail.com>
2019-01-22 20:59:46 +0700

    add repository -close #55

--- backend/src/main/java/sut/se/G09/Backend/Repository/AgentRegistrationRepository.java
+++ backend/src/main/java/sut/se/G09/Backend/Repository/AgentRegistrationRepository.java
@@ -1,8 +1,9 @@
 package sut.se.G09.Backend.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import sut.se.G09.Backend.Entity.Province;
+import sut.se.G09.Backend.Entity.AgentRegistration;
 
 @RepositoryRestResource
-public interface ProvinceRepository extends JpaRepository<Province, Long> {
-    Province findByID(Long iD);}
+public interface AgentRegistrationRepository extends JpaRepository<AgentRegistration, Long> {
+  AgentRegistration findByID(Long iD);
+}
--- backend/src/main/java/sut/se/G09/Backend/Repository/Educationalrepository.java
+++ backend/src/main/java/sut/se/G09/Backend/Repository/Educationalrepository.java
@@ -1,8 +1,10 @@
 package sut.se.G09.Backend.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import sut.se.G09.Backend.Entity.Province;
+import sut.se.G09.Backend.Entity.Educational;
 
 @RepositoryRestResource
-public interface ProvinceRepository extends JpaRepository<Province, Long> {
-    Province findByID(Long iD);}
+public interface Educationalrepository extends JpaRepository<Educational, Long> {
+       Educational findByID(long iD);
+}
+

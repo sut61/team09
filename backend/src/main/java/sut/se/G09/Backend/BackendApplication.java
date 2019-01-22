937d7d47389eed6f292da9f023fee4b902ebc7bb
U-DESKTOP-34HAJ6U\RUGGED 7214 <b5906192a@gmail.com>
2019-01-22 11:29:22 +0700

    update Backend BaseFile

---
+++ backend/src/main/java/sut/se/G09/Backend/BackendApplication.java
@@ -0,0 +1,14 @@
+package sut.se.G09.Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
+

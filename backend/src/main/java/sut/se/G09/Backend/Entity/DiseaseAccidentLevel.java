c094fa23da1c229c540dc502e149e0f26adefdbd
isaraarisa <Isara797@gmail.com>
2019-01-22 18:20:26 +0700

    ทำ Entity ของ ระบบย่อย ข้อมูลโรคและอุบัติเหตุที่ได้รับสิทธ์ิคุ้มครอง - close #34

---
+++ backend/src/main/java/sut/se/G09/Backend/Entity/DiseaseAccidentLevel.java
@@ -0,0 +1,30 @@
+package sut.se.G09.Backend.Entity;
+import lombok.*;
+import javax.persistence.*;
+
+@Entity  //บอกว่าเป็น class entity class ที่เก็บขอมูล
+@Data  // lombox จะสร้าง method getter setter ให้เอง
+@Getter @Setter
+
+@ToString
+@Table(name="Level") //ชื่อตาราง
+public class DiseaseAccidentLevel {
+
+  @Id  //  Annotations  @Id  บอกว่าเป็น  Primary  key
+@SequenceGenerator(name="DiseaseAccidentLevel_seq",sequenceName="DiseaseAccidentLevel_seq")
+@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="DiseaseAccidentLevel_seq")
+@Column(name="Level_ID",unique = true, nullable = true)
+private @NonNull long id;
+private @NonNull String levelText;
+
+
+  public DiseaseAccidentLevel(){}
+
+
+
+
+  public void setLevelText(String levelText) {
+    this.levelText = levelText;
+
+  }
+}

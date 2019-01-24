ca353617665f74f5501cb3f260f902b4b62321e5
tongpisztt <pisztt@gmail.com>
2019-01-24 22:16:11 +0700

    เพิ่มไฟล์ backend ของระบบนัดพบตัวแทนประกันชีวิต -close #26

---
+++ frontend/src/app/agent-appointment/agent-appointment.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import { AppointmentService } from '../service/appointment.service';
+import { HttpClient } from '@angular/common/http';
+import { Router} from '@angular/router';
+
+@Component({
+  selector: 'app-agent-appointment',
+  templateUrl: './agent-appointment.component.html',
+  styleUrls: ['./agent-appointment.component.css']
+})
+export class AgentAppointmentComponent implements OnInit {
+Categories : Array<any>;
+Genders: Array<any>;
+Date : Array<any>;
+Duration : Array<any>;
+Provinces : Array<any>;
+Appointment : any = {
+typeName: '', fName: '', lName: '', genderName: '', age: '', telNum: ''
+,email: '' , provinceName: '', date: '',  duration: ''};
+
+constructor(private appointService : AppointmentService ,private httpClient: HttpClient, private router:Router) { }
+  ngOnInit() {
+    this.appointService.getAppointment().subscribe(data => { this.Appointment = data; console.log(this.Appointment);});
+    this.appointService.getCategory().subscribe(dataF => { this.Categories = dataF; console.log(this.Categories);});
+    this.appointService.getGender().subscribe(dataB => { this.Genders = dataB; console.log(this.Genders);});
+    this.appointService.getDateAppointment().subscribe(dataC => { this.Date = dataC; console.log(this.Date);});
+    this.appointService.getDurationAppointment().subscribe(dataD => { this.Duration = dataD; console.log(this.Duration);});
+    this.appointService.getProvince().subscribe(dataE => { this.Provinces = dataE; console.log(this.Provinces);});   }
+
+   save() {
+     if (this.Appointment.typeName == null || this.Appointment.fName == null || this.Appointment.lName == null ||
+        this.Appointment.genderName == null || this.Appointment.age == null || this.Appointment.telNum == null ||
+        this.Appointment.email == null || this.Appointment.provinceName == null || this.Appointment.date == null ||
+        this.Appointment.duration == null)
+        {alert('กรุณากรอกข้อมูลให้ครบถ้วน');}
+
+      else{
+        this.httpClient.post('http://localhost:8080/MakeAppointment/' + this.Appointment.typeName + '/'
+        + this.Appointment.fName + '/' + this.Appointment.lName + '/' + this.Appointment.genderName + '/'
+        + this.Appointment.age + '/' + this.Appointment.telNum + '/' + this.Appointment.email + '/'
+        + this.Appointment.provinceName + '/'+ this.Appointment.date + '/' + this.Appointment.duration
+        ,this.Appointment)
+          .subscribe(
+             data => {
+             console.log(this.Appointment)
+              const Appointment  = this.Appointment
+              this.router.navigate(['showAppointmentResult',{typeName:Appointment.typeName, fName:Appointment.fName
+              , lName:Appointment.lName, genderName :this.Appointment.genderName, age:Appointment.age
+              , telNum:Appointment.telNum, email:Appointment.email, provinceName:Appointment.provinceName
+              , date:Appointment.date, duration:Appointment.duration}])
+              console.log('PUT Request is successful', data);
+              {alert('ลงทะเบียนสำเร็จ!');}
+
+             },
+             error => { console.log('Rrror', error); }
+          );               alert('ลงทะเบียนสำเร็จ!');
+
+        }
+    }
+}
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,13 +4,49 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { Component } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { NoopAnimationsModule} from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCardModule, MatInputModule, MatListModule, MatToolbarModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDialogModule} from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDividerModule } from '@angular/material/divider';
+
+import { RouterModule, Routes } from '@angular/router';
+import { AppointmentService } from './service/appointment.service';
+import { AgentAppointmentComponent } from './agent-appointment/agent-appointment.component';
+import { ShowAppointmentResultComponent } from './show-appointment-result/show-appointment-result.component';
+
+const appRoutes: Routes = [
+
+];
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AgentAppointmentComponent,
+    ShowAppointmentResultComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+HttpClientModule,
+BrowserAnimationsModule,
+NoopAnimationsModule,
+MatSelectModule,
+MatDialogModule,
+MatFormFieldModule,
+MatButtonModule,
+MatCardModule,
+MatInputModule,
+MatListModule,
+MatToolbarModule,
+MatDividerModule,
+FormsModule,
+RouterModule.forRoot(appRoutes),
+AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]

dbddabfcabd1b978a096603386ebe975afbaad47
isaraarisa <Isara797@gmail.com>
2019-01-25 13:25:25 +0700

    ทำ DEBUG ทั้งระบบ by ISARA - close #81

---
+++ DEBUG/src/main/java/sut/se/G09/Backend/Entity/DiseaseAccidentType.java
@@ -0,0 +1,30 @@
+package sut.se.G09.Backend.Entity;
+import lombok.*;
+import javax.persistence.*;
+
+@Entity  //บอกว่าเป็น class entity class ที่เก็บขอมูล
+@Data  // lombox จะสร้าง method getter setter ให้เอง
+@Getter @Setter
+
+@ToString
+@Table(name="Type") //ชื่อตาราง
+public class DiseaseAccidentType {
+
+  @Id  //  Annotations  @Id  บอกว่าเป็น  Primary  key
+@SequenceGenerator(name="DiseaseAccidentType_seq",sequenceName="DiseaseAccidentType_seq")
+@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="DiseaseAccidentType_seq")
+@Column(name="Type_ID",unique = true, nullable = true)
+private @NonNull long id;
+private @NonNull String typeText;
+
+
+  public DiseaseAccidentType(){}
+
+
+
+
+  public void setDiseaseAccidentType(String typeText) {
+    this.typeText = typeText;
+
+  }
+}

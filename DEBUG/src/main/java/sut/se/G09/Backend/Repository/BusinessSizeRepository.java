81a1ab3515f876f38cadc9e3d1defcd2b1bd8d84
tanchananya <b5914814@gmail.com>
2019-01-25 17:27:24 +0700

    DEBUG Chananya -close #81

---
+++ DEBUG/src/main/java/sut/se/G09/Backend/Repository/BusinessSizeRepository.java
@@ -0,0 +1,10 @@
+package sut.se.G09.Backend.Repository;
+
+import sut.se.G09.Backend.Entity.BusinessSize;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+@RepositoryRestResource
+public interface BusinessSizeRepository extends JpaRepository<BusinessSize, Long>{
+    BusinessSize findByID(long id);
+}
---
+++ DEBUG/src/main/java/sut/se/G09/Backend/Repository/LumpsumRepository.java
@@ -0,0 +1,8 @@
+package sut.se.G09.Backend.Repository;
+
+import sut.se.G09.Backend.Entity.Lumpsum;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+@RepositoryRestResource
+public interface LumpsumRepository extends JpaRepository<Lumpsum, Long>{
+}

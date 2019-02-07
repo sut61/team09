70f352823d64b67d278d87a67d4fcd6a8849473d
tongpisztt <pisztt@gmail.com>
2019-02-07 19:37:11 +0700

    เพิ่ม Repository และอัพเดท Entity -close #86

---
+++ backend/src/main/java/sut/se/G09/Backend/Repository/CancelAppointmentReasonRepository.java
@@ -0,0 +1,10 @@
+package sut.se.G09.Backend.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import sut.se.G09.Backend.Entity.CancelAppointment;
+
+@RepositoryRestResource
+public interface CancelAppointmentReasonRepository extends JpaRepository<CancelAppointment, Long> {
+
+}

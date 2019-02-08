e7f9655d3cb4fdc588773c81a1756f69093b517b
isaraarisa <Isara797@gmail.com>
2019-02-08 18:55:16 +0700

    ทำ การแก้หน้า โชว์ข้อมูลของข้อมูลโรคและอุบัติเหตุที่ได้รับสิทธิ์คุ้มครอง - close #116

--- frontend/src/app/DiseaseAccidentDataUI/DiseaseAccidentDataUI.component.ts
+++ frontend/src/app/DiseaseAccidentDataUI/DiseaseAccidentDataUI.component.ts
@@ -36,21 +36,23 @@ dataNameAdd :''  ,typeIdSelect :'' ,levelIdSelect :'',medicalFeeIdSelect :''
                 this.medicalFeeIdA = data3;
                 console.log(this.medicalFeeIdA);
               });
-
+/**
    this.Service.getDiseaseAccidentData().subscribe(data => {
              this.DiseaseAccidentData = data;
              console.log(this.DiseaseAccidentData);
            });
-
+*/
 
   }
 
   /**====SAVE====*/
   save(){
-    if(this.DiseaseAccidentData.dataNameAdd === ''||this.DiseaseAccidentData.levelIdSelect === ''||
-    this.DiseaseAccidentData.typeIdSelect === ''||this.DiseaseAccidentData.medicalFeeIdSelect === ''){
 
-      alert('กรุณาเลือกข้อมูลให้ครบ');
+    if(this.DiseaseAccidentData.dataNameAdd === ''|| this.DiseaseAccidentData.typeIdSelect === ''||
+    this.DiseaseAccidentData.levelIdSelect === ''|| this.DiseaseAccidentData.medicalFeeIdSelect === ''){
+
+     alert('กรุณาเลือกข้อมูลให้ครบ!');
+
 
     }else{
 
@@ -65,7 +67,6 @@ dataNameAdd :''  ,typeIdSelect :'' ,levelIdSelect :'',medicalFeeIdSelect :''
 
 
         data => {
-        {alert('ลงทะเบียนสำเร็จ!');}
         console.log(this.DiseaseAccidentData)
                                const DiseaseAccidentData  = this.DiseaseAccidentData
                                this.router.navigate(['ShowDiseaseAccidentDataResult',{
@@ -74,12 +75,12 @@ dataNameAdd :''  ,typeIdSelect :'' ,levelIdSelect :'',medicalFeeIdSelect :''
                                levelIdSelect:DiseaseAccidentData.typeIdSelect,
                                medicalFeeIdSelect:DiseaseAccidentData.medicalFeeIdSelect}])
                                console.log('PUT Request is successful', data);
-
-        },
+                               {alert('ลงทะเบียนสำเร็จ!');}
+              },
 
         error => {
 
-         error => { console.log('Error', error); }
+         error => { console.log('Error', error);   }
 
         }
       );
